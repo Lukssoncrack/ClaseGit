77b8f51c0d67db4d65f4032cff3f0a3a0cda062e
maximochialvo <mchialvo@udesa.edu.ar>
2024-11-15 11:36:37 -0300

    Fetch de las recetas

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
-hola
-"salame"
+fetch('https://dummyjson.com/recipes')
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data)
+})
+.catch(function(error){
+    console.log(error)
+})
