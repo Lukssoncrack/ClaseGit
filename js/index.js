96a1ee782bcddf0998952129fdecfff997f3f64a
lsilveyrastengel@udesa.edu.ar <lsilveyrastengel@udesa.edu.ar>
2024-11-19 20:50:02 -0300

    APIs home/index

--- js/index.js
+++ js/index.js
@@ -1,10 +1,42 @@
+
+
 fetch('https://dummyjson.com/recipes')
 .then(function(response){
-    return response.json()
+    return response.json();
 })
 .then(function(data){
-    console.log(data)
+    console.log(data);
+    let listarecetas = document.querySelector(".listarecetas")
+    let recetas =""
+
+    for (let i = 0; i < 10; i++){
+        console.log(i)
+        recetas += `
+          <article class="fotoReceta">
+            <img src=${data.recipes[i].image} alt="">
+            <article class ="platos"><a class"platoa" href="./index.html?id=${data.recipes[i].id}">
+            Plato: ${data.recipes[i].name}</a>
+            <p class="difcil">Nivel de dificultad: ${data.recipes[i].difficulty}</p></article>
+          </article>`;
+    }
+    listarecetas.innerHTML=recetas
+
+    let mass = document.querySelector(".buttonMas")
+    mass.addEventListener('click', function(){
+        for(let i=10;i<20;i++){
+            recetas += `<article class="fotoReceta">
+            <img src=${data.recipes[i].image} alt="" ><div>
+            <a class="platoa" href="./index.html?id=${data.recipes[i].id}">
+            Plato: ${data.recipes[i].name}</a>
+             <p class="difcil">Nivel de dificultad: ${data.recipes[i].difficulty}</p></div>
+          </article>`;
+          listarecetas.innerHTML=recetas
+        } 
+    })
+    
 })
+
+
 .catch(function(error){
     console.log(error)
 })
