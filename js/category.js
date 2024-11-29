0211357c4b637c29ec6589c2619abfe086c1d665
Tomasgds <tgonzalezdelsolar@udesa.edu.ar>
2024-11-29 10:33:21 -0300

    Terminado el css de category y retocado un poco el java

--- js/category.js
+++ js/category.js
@@ -13,9 +13,11 @@ fetch(`https://dummyjson.com/recipes/tag/${detalle}`)
     for (let i=0; i<data.recipes.length; i++){
         if(data.recipes[i].tags.includes(detalle)){
             categories += `<article class="articleDC">
+
+        
+        <article class="article_foto_plato"><h3 class="plato" href="./receta.html?id=${data.recipes[i].id}">
+         ${data.recipes[i].name}</h3>
         <img class="imgDC" src=${data.recipes[i].image} alt = "" >
-        <article class="article_foto_plato"><a class="plato" href="./receta.html?id=${data.recipes[i].id}">
-        Plato: ${data.recipes[i].name}</a>
         <p class="dificultad" >Dificultad: ${data.recipes[i].difficulty}</p></article>
         </article>`} 
     
