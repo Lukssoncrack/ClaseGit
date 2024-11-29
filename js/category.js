720483c2c944fd81c260cd46a11965a756133a2e
maximochialvo <mchialvo@udesa.edu.ar>
2024-11-28 23:06:07 -0300

    category casi cocinado y pequena modificacion en el categories

--- js/category.js
+++ js/category.js
@@ -1,4 +1,8 @@
-fetch('https://dummyjson.com/recipes/tag/Pakistani')
+let queryString = location.search
+let queryStringObjeto = new URLSearchParams(queryString)
+let detalle = queryStringObjeto.get("tags")
+
+fetch(`https://dummyjson.com/recipes/tag/Pakistani/${detalle}`)
 .then(function(response){
     return response.json();
 })
@@ -6,14 +10,21 @@ fetch('https://dummyjson.com/recipes/tag/Pakistani')
     console.log(data);
     let category = document.querySelector(".category")
 
+    for (let i=0; i<data.recipes.length; i++){
+        if(data.recipes[i].tags.includes(detalle)){
+            category += `<article class="articleDC">
+        <img class="imgDC" src=${data.recipes[i].image} alt = "" >
+        <article class="article_foto_plato"><a class="plato" href="./receta.html?id=${data.recipes[i].id}">
+        Plato: ${data.recipes[i].name}</a>
+        <p class="dificultad" >Dificultad: ${data.recipes[i].difficulty}</p></article>
+        </article>`} 
+    
+    seccion_cat.innerHTML = categories
+}})
 
-})
+
 
 .catch(function(error){
     console.log(error)
 })
-
-
-
-
 
