69cf97204efdb4c8ac4becccb166b055bb57014d
Tomasgds <tgonzalezdelsolar@udesa.edu.ar>
2024-11-29 10:47:37 -0300

    corrigiendo error

--- js/category.js
+++ js/category.js
@@ -15,8 +15,10 @@ fetch(`https://dummyjson.com/recipes/tag/${detalle}`)
             categories += `<article class="articleDC">
 
         
-        <article class="article_foto_plato"><h3 class="plato" href="./receta.html?id=${data.recipes[i].id}">
-         ${data.recipes[i].name}</h3>
+        <article class="article_foto_plato">
+        <a class="plato" href="./receta.html?id=${data.recipes[i].id}">
+         ${data.recipes[i].name}</a>
+         
         <img class="imgDC" src=${data.recipes[i].image} alt = "" >
         <p class="dificultad" >Dificultad: ${data.recipes[i].difficulty}</p></article>
         </article>`} 
